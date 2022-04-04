828795dbf98887ebe5084be6f55fa86b74aaf508
April <aprilnicole013@gmail.com>
2022-04-04 15:49:57 -0400

    Display launch data and clickable links

--- pages/index.js
+++ pages/index.js
@@ -23,33 +23,15 @@ export default function Home({ launches }) {
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          {launches.map((launch) => {
+            return (
+              <a key={ launch.id } href={ launch.links.video_link } className={styles.card}>
+                <h2>{ launch.mission_name }</h2>
+                <p><strong>Launch Time: </strong>{ new Date(launch.launch_date_local).toLocaleDateString("en-US")}</p>
+              </a>
+            )
+          })}
+          
         </div>
       </main>
 
